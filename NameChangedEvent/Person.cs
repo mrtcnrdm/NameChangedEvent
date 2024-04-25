b62bdeda576d10416add6b0a2e2edfe47dbdf765
Mert Can Erdem <mrtcnrdmii@gmail.com>
2024-04-25 14:14:07 +0300

    Initial commit

---
+++ NameChangedEvent/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace NameChangedEvent
+{
+	public class Person
+	{
+        private string name; // Eski değeri tutar
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                // Eğer yeni değer eski değerden farklıysa, önceki değeri kontrol eden ve yeni değeri atayan event'i tetikle
+                if (value != name)
+                {
+                    OnNameChanged(name, value);
+                    name = value;
+                }
+            }
+        }
+
+        // NameChanged event'i
+        public event EventHandler<NameChangedEventArgs> NameChanged;
+
+        // Event'i tetikleyen yardımcı metod
+        protected virtual void OnNameChanged(string oldName, string newName)
+        {
+            NameChanged?.Invoke(this, new NameChangedEventArgs(oldName, newName));
+        }
+    }
+}
+
+// NameChanged event'inin veri yapısı
+public class NameChangedEventArgs : EventArgs
+{
+    public string OldName { get; }
+    public string NewName { get; }
+
+    public NameChangedEventArgs(string oldName, string newName)
+    {
+        OldName = oldName;
+        NewName = newName;
+    }
+}
---
+++ NameChangedEvent/Program.cs
@@ -0,0 +1,20 @@
+﻿using NameChangedEvent;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        // Event'e bir handler ekleme
+        person.NameChanged += PersonNameChanged!;
+        person.Name = "Mert"; // Default boş geldiği için ek burada da çalışacak
+        person.Name = "Mert Can";
+    }
+
+    // Event handler metodu
+    private static void PersonNameChanged(object sender, NameChangedEventArgs e)
+    {
+        Console.WriteLine($"İsim değiştirildi, eski değer: '{e.OldName}', yeni değer: '{e.NewName}'.");
+    }
+}
